e818b4fe94e2082887247b98ba4fcc4c534d0783
ion100 <ion100@mail.ru>
2015-10-23 15:50:32 +0500

    Rejected commit: Default

--- app/src/main/java/orm/ISession.java
+++ app/src/main/java/orm/ISession.java
@@ -8,14 +8,14 @@ public interface ISession {
     <T> int update(T item);
     <T> int insert(T item);
     <T> int delete(T item);
-    <T> List<T> getList(Class<T> tClass, String where, Object... objectses) ;
+    <T> List<T> getList(Class<T> tClass, String where, Object... objects) ;
 
     <T> T get(Class<T> tClass, Object id);
     <T> Object executeScalar(String sql, Object ... objects);
     void execSQL(String sql, Object ... objects);
     void beginTransaction();
     void commitTransaction();
-    void endTaransaction();
+    void endTransaction();
     void close();
 
 
